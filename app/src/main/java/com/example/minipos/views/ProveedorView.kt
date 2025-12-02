b60b671ff3ee28acd3090dffa5ca0973c92bdcc6
AbrahamPadrino <abrahampadrino@gmail.com>
2025-12-02 11:25:17 -0500

    Agregado: Scaffold a ProveedorView

--- app/src/main/java/com/example/minipos/views/ProveedorView.kt
+++ app/src/main/java/com/example/minipos/views/ProveedorView.kt
@@ -1,12 +1,31 @@
 package com.example.minipos.views
 
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.minipos.R
 import com.example.minipos.models.Proveedor
 import com.example.minipos.viewmodels.ProveedorViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProveedorView(
     navController: NavController,
@@ -15,5 +34,63 @@ fun ProveedorView(
     val openDialog = remember { mutableStateOf(false) }
     val proveedorAddUpdate = remember { mutableStateOf("") }
     val proveedor = remember { mutableStateOf(Proveedor()) }
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "PROVEEDORES")
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.navigateUp()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "regresar"
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier
+                    .width(30.dp)
+                    .height(30.dp),
+                onClick = {
+                    proveedorAddUpdate.value = "add"
+                    openDialog.value = true
+                },
+                containerColor = colorResource(id = R.color.gris_oscuro)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    tint = Color.White,
+                    contentDescription = null
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+    ) {
+        if (openDialog.value) {
+            DialogAddUpdateProveedor(
+                openDialog = openDialog,
+                proveedorAddUpdate = proveedorAddUpdate,
+                viewModel = viewModel,
+                proveedor = proveedor
+            )
+        }
+
+        ProveedoresScreen(
+            paddingValues = it,
+            openDialog = openDialog,
+            proveedorAddUpdate = proveedorAddUpdate,
+            proveedor = proveedor,
+            viewModel = viewModel
+        )
+    }
 
 }
