bd2a7c747b0fa86792855b20de334347241c1343
Abraham <abraham@Mac-mini-de-Abraham.local>
2025-12-02 11:10:39 -0500

    Definido: Estados para ProveedorView

--- app/src/main/java/com/example/minipos/views/ProveedorView.kt
+++ app/src/main/java/com/example/minipos/views/ProveedorView.kt
@@ -1,8 +1,19 @@
 package com.example.minipos.views
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.navigation.NavController
+import com.example.minipos.models.Proveedor
+import com.example.minipos.viewmodels.ProveedorViewModel
 
 @Composable
-fun ProveedorView() {
+fun ProveedorView(
+    navController: NavController,
+    viewModel: ProveedorViewModel
+) {
+    val openDialog = remember { mutableStateOf(false) }
+    val proveedorAddUpdate = remember { mutableStateOf("") }
+    val proveedor = remember { mutableStateOf(Proveedor()) }
 
 }
